a509892b05b5570d9ad0e34ddf3a336ef87223da
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-10-09 20:08:31 +0500

    Added OpenStreetMap tiles

--- public/js/script.js
+++ public/js/script.js
@@ -17,4 +17,9 @@ if(navigator.geolocation){
     )
 }
 
-L.map('map').setView([0, 0], 10)
+const map = L.map('map').setView([0, 0], 10)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: 'Azmeer Hassan Ammad'
+}).addTo(map)
+
